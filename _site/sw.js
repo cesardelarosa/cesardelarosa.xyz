e209786cc58369b08bf7418e73a424a0bfa1c538
César de la Rosa <cesardelarosa.code@proton.me>
2024-02-11 16:31:01 +0100

    ignore

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707665386';
+const cacheName = 'chirpy-1707665441';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
