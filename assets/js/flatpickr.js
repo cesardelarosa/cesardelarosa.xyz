b84be953fd8dd428bffb9be08ce75f9cf268ab12
César de la Rosa <code@cesardelarosa.xyz>
2024-03-12 17:07:24 +0100

    es

--- assets/js/flatpickr.js
+++ assets/js/flatpickr.js
@@ -1,9 +1,11 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from "flatpickr";
+import es from "flatpickr/dist/l10n/es.js"; // Importa el archivo de idioma español
 
 document.addEventListener('DOMContentLoaded', function() {
     flatpickr("#fecha-selector", {
-        inline: true, // Esto hace que el calendario aparezca siempre
+        inline: true,
+        locale: es, // Configura el idioma español
+        weekStart: 1, // Configura para que las semanas empiecen en lunes (0 para domingo, 1 para lunes, etc.)
         onChange: function(selectedDates, dateStr, instance) {
             redirectToPage(dateStr);
         }
