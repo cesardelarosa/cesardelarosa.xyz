f3a3361cabe5ea4914f738d2a17eae4f6fa4e358
César de la Rosa <code@cesardelarosa.xyz>
2024-03-14 00:54:36 +0100

    manual

--- assets/js/flatpickr.js
+++ assets/js/flatpickr.js
@@ -1,15 +1,24 @@
 import flatpickr from "flatpickr";
-import { Spanish } from "flatpickr/dist/l10n/es.js";
 
 document.addEventListener('DOMContentLoaded', function() {
     flatpickr("#fecha-selector", {
 	maxDate: "today",
 	minDate: "2024-01-01",
         inline: true,
-	locale: "Spanish",
+	locale: {
+		firstDayOfWeek: 1,
+		weekdays: {
+			shorthand: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+			longhand: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+		},
+		months: {
+			shorthand: ["Ene", "Fed", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+			longhand: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+		},
+	},
         onChange: function(selectedDates, dateStr, instance) {
             redirectToPage(dateStr);
-        }
+        },
     });
 
     function redirectToPage(dateStr) {
