0d21a96601ebe114639d65015a0d8cf2ddd457bd
César de la Rosa <code@cesardelarosa.xyz>
2024-03-17 18:46:40 +0100

    some changes

--- assets/js/flatpickr.js
+++ assets/js/flatpickr.js
@@ -1,24 +1,25 @@
 const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const theme = isDarkMode ? "dark" : "light";
+
 import flatpickr from "flatpickr";
 import ("flatpickr/dist/themes/" + theme + ".css");
 
 document.addEventListener('DOMContentLoaded', function() {
     flatpickr("#fecha-selector", {
-	maxDate: "today",
-	minDate: "2024-01-01",
+        maxDate: "today",
+        minDate: "2024-01-01",
         inline: true,
-	locale: {
-		firstDayOfWeek: 1,
-		weekdays: {
-			shorthand: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
-			longhand: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
-		},
-		months: {
-			shorthand: ["Ene", "Fed", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
-			longhand: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-		},
-	},
+        locale: {
+            firstDayOfWeek: 1,
+            weekdays: {
+                shorthand: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+                longhand: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+            },
+            months: {
+                shorthand: ["Ene", "Fed", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+                longhand: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+            },
+        },
         onChange: function(selectedDates, dateStr, instance) {
             redirectToPage(dateStr);
         },
